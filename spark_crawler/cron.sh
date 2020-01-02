adf86b309c703dcf2b256f08fdba8dfc737822ea
Andy616 <53362130+Andy616@users.noreply.github.com>
2020-01-02 19:24:08 +0800

    Update cron.sh

--- spark_crawler/cron.sh
+++ spark_crawler/cron.sh
@@ -1,4 +1,4 @@
-# set crontab: * /1 * * * cron.sh
+# set crontab: @hourly cron.sh
 
 spark-submit --master spark://spark-master:7077 /pyfile/walkerland.py
 spark-submit --master spark://spark-master:7077 /pyfile/ifood.py
